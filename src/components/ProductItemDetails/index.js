be4f206eadd601c829feea4f2811f1ad63cacca4
manojbachu <135940834+manojbachu@users.noreply.github.com>
2023-09-08 09:43:28 +0530

    solved

--- src/components/ProductItemDetails/index.js
+++ src/components/ProductItemDetails/index.js
@@ -1,17 +1,27 @@
+// Write your code here
 import {Component} from 'react'
+import Loader from 'react-loader-spinner'
 import Cookies from 'js-cookie'
+import {BsPlusSquare, BsDashSquare} from 'react-icons/bs'
 import Header from '../Header'
 import SimilarProductItem from '../SimilarProductItem'
 import './index.css'
 
 class ProductItemDetails extends Component {
-  state = {count: 1, productDetails: {}, similarProductList: []}
+  state = {
+    count: 1,
+    productDetails: {},
+    similarProductList: [],
+    isLoading: false,
+    errorMsg: '',
+  }
 
   componentDidMount() {
     this.getProductDetails()
   }
 
   getProductDetails = async () => {
+    this.setState({isLoading: true})
     const {match} = this.props
     const {params} = match
     const {id} = params
@@ -25,17 +35,24 @@ class ProductItemDetails extends Component {
     }
 
     const response = await fetch(apiUrl, options)
-    const data = await response.json()
-    const productDetails = this.convertSnakeToCamelCase(data)
-    const productList = {similarProducts: data.similar_products}
 
-    this.setState({
-      productDetails,
-      similarProductList: productList.similarProducts.map(eacdData => {
-        const convertedData = this.convertSnakeToCamelCase(eacdData)
-        return convertedData
-      }),
-    })
+    const data = await response.json()
+    if (response.ok) {
+      const productDetails = this.convertSnakeToCamelCase(data)
+      const productList = {similarProducts: data.similar_products}
+
+      this.setState({
+        isLoading: false,
+        productDetails,
+        similarProductList: productList.similarProducts.map(eacdData => {
+          const convertedData = this.convertSnakeToCamelCase(eacdData)
+          return convertedData
+        }),
+      })
+    } else {
+      const error = {errorMsg: data.error_msg}
+      this.setState({errorMsg: error.errorMsg, isLoading: false})
+    }
   }
 
   convertSnakeToCamelCase = data => ({
@@ -50,6 +67,11 @@ class ProductItemDetails extends Component {
     title: data.title,
     totalReviews: data.total_reviews,
   })
+
+  onClickContinueShopping = () => {
+    const {history} = this.props
+    history.replace('/products')
+  }
 
   onClickDecrease = () => {
     const {count} = this.state
@@ -83,7 +105,6 @@ class ProductItemDetails extends Component {
 
     const {
       imageUrl,
-      name,
       title,
       price,
       rating,
@@ -95,7 +116,7 @@ class ProductItemDetails extends Component {
 
     return (
       <div className="product-details-container">
-        <img className="product-image" src={imageUrl} alt={name} />
+        <img className="product-image" src={imageUrl} alt="product" />
         <div className="product-details">
           <h1 className="product-title">{title}</h1>
           <p className="product-price">Rs {price}/-</p>
@@ -112,27 +133,29 @@ class ProductItemDetails extends Component {
           </div>
           <p className="product-description">{description}</p>
           <p className="available">
-            Available: <span className="availability">{availability}</span>
+            Available: <p className="availability">{availability}</p>
           </p>
           <p className="available">
-            Brand: <span className="availability">{brand}</span>
+            Brand: <p className="availability">{brand}</p>
           </p>
           <hr className="break-line" />
           <div className="count-container">
             <button
+              data-testid="minus"
               onClick={this.onClickDecrease}
               type="button"
               className="count-button"
             >
-              -
+              <BsDashSquare />
             </button>
             <p className="count-number">{count}</p>
             <button
+              data-testid="plus"
               onClick={this.onClickIncrease}
               type="button"
               className="count-button"
             >
-              +
+              <BsPlusSquare />
             </button>
           </div>
           <button className="add-to-cart-button" type="button">
@@ -143,12 +166,55 @@ class ProductItemDetails extends Component {
     )
   }
 
+  renderLoading = () => (
+    <div data-testid="loader" className="loader">
+      <Loader type="ThreeDots" color="#0b69ff" height={80} width={80} />
+    </div>
+  )
+
+  renderFailureView = () => {
+    const {errorMsg} = this.state
+    return (
+      <div className="product-items-failure">
+        <img
+          className="error-image"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-error-view-img.png"
+          alt="failure view"
+        />
+        <h1 className="error-msg">{errorMsg}</h1>
+        <button
+          onClick={this.onClickContinueShopping}
+          className="continue-button"
+          type="button"
+        >
+          Continue Shopping
+        </button>
+      </div>
+    )
+  }
+
+  renderProductItemDetails = () => {
+    const {errorMsg} = this.state
+    return (
+      <>
+        {errorMsg === '' ? (
+          <>
+            {this.renderProductDetails()} {this.renderSimilarProducts()}
+          </>
+        ) : (
+          this.renderFailureView()
+        )}
+      </>
+    )
+  }
+
   render() {
+    const {isLoading} = this.state
     return (
       <>
         <Header />
-        {this.renderProductDetails()}
-        {this.renderSimilarProducts()}
+
+        {isLoading ? this.renderLoading() : this.renderProductItemDetails()}
       </>
     )
   }
